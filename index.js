adf22a012be283e0841e002ac9be7a36bd407c3a
LRoGlez <luis.roglez@gmail.com>
2020-11-11 10:35:47 +0100

    2 files modified

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // CONTENIDO DE LA PRÁCTICA:
-// Vamos a añadir elementos en una lista (con la clase "color-list") con javascript a partir del array aportado en este documento, en la constante "colorList" (ver imagen en el proyecto "ejemplo_lista.png"). 
+// Vamos a añadir elementos en una lista (con la clase "color-list") con javascript a partir del array aportado en este documento, en la constante "colorList" (ver imagen en el proyecto "ejemplo_lista.png").
 
 // Como se puede apreciar en la imagen, cada elemento que esté en una posición par de de la lista tiene que tener la clase "color-item--odd". Esta clase debe añadirse desde javascript, NO haciendo uso del selector css nth-of-type(odd) o similares. NOTA: En este caso vamos a considerar un elemento par pensando en el primer elemento como el 1 no como el 0.
 
@@ -28,35 +28,68 @@
 
 const colorList = [
   {
-    colorName: 'white',
-    hex: '#ffffff'
+    colorName: "white",
+    hex: "#ffffff"
   },
   {
-    colorName: 'red',
-    hex: '#ff0000'
+    colorName: "red",
+    hex: "#ff0000"
   },
   {
-    colorName: 'orange',
-    hex: '#ffa500'
+    colorName: "orange",
+    hex: "#ffa500"
   },
   {
-    colorName: 'yellow',
-    hex: '#ffff00'
+    colorName: "yellow",
+    hex: "#ffff00"
   },
   {
-    colorName: 'orchid',
-    hex: '#da70d6'
+    colorName: "orchid",
+    hex: "#da70d6"
   },
   {
-    colorName: 'pink',
-    hex: '#ffc0cb'
+    colorName: "pink",
+    hex: "#ffc0cb"
   },
   {
-    colorName: 'green',
-    hex: '#008000'
+    colorName: "green",
+    hex: "#008000"
   },
   {
-    colorName: 'silver',
-    hex: '#c0c0c0'
+    colorName: "silver",
+    hex: "#c0c0c0"
   }
 ];
+
+let item = document.createElement("li");
+let item2 = document.createElement("li");
+let item3 = document.createElement("li");
+let item4 = document.createElement("li");
+let item5 = document.createElement("li");
+let item6 = document.createElement("li");
+let item7 = document.createElement("li");
+let item8 = document.createElement("li");
+
+let empezar = document.querySelector(".color-list-title");
+console.log(empezar);
+empezar.appendChild(item);
+empezar.appendChild(item2);
+empezar.appendChild(item3);
+empezar.appendChild(item4);
+empezar.appendChild(item5);
+empezar.appendChild(item6);
+empezar.appendChild(item7);
+empezar.appendChild(item8);
+
+let divNWhite = document.createElement("div");
+let divMWhite = document.createElement("div");
+let button1w = document.createElement("button");
+button1w.type = "button";
+button1w.innerText = "next item color";
+let button2w = document.createElement("button");
+button2w.type = "button";
+button2w.innerText = "page color";
+
+for (let i = 0; i < empezar.length; i++) {
+  empezar[i].classList.add("color-list");
+}
